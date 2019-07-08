1bf285fe64e7445bb77c51ef60dfd32e6508af77
bokoblin-link <alexgagnon227@gmail.com>
2019-07-07 23:09:06 -0400

    Added custom object representation. Updated path to save excel file on desktop. Updated spacing for columns & rows. Added unit tests.

--- excel_scripts/create_excel_file.py
+++ excel_scripts/create_excel_file.py
@@ -1,7 +1,9 @@
+import os
+
 import xlwt as excel
 
 
-class CreateExcel:
+class CreateExcelFile:
     def __init__(self, *args, **kwargs):
         self.args = args
         self.book = excel.Workbook()
@@ -20,18 +22,21 @@ class CreateExcel:
 
         print(f'Workbook filename and sheetname redefined as: filename={self.filename}, sheetname={self.sheetname}')
 
+    def __repr__(self):
+        return f'<CreateExcelFile Object: filename={self.filename}, sheetname={self.sheetname}">'
+
     def create_excel_book(self, data_list):
         print('Generating report...')
         self.write_data(data_list=data_list)
         self.save_and_close()
 
-    def write_data(self, data_list, n=0):
-        n += 1
+    def write_data(self, data_list):
         for dict_item in data_list:
             for n, (k, v) in enumerate(dict_item.items()):
-                self.sheet.write(0, n, k)
-                self.sheet.write(1, n, v)
+                self.sheet.write(0, n * 2, k)
+                self.sheet.write(2, n * 2, v)
 
     def save_and_close(self):
-        self.book.save(self.filename + '.xls')
+        file_path = os.path.expanduser(os.path.join('~', 'Desktop'))
+        self.book.save(os.path.join(file_path, self.filename + '.xls'))
         print(f'Report finished. Excel file saved as "{self.filename}.xls"')
---
+++ test/test_create_excel_file.py
@@ -0,0 +1,15 @@
+import pytest
+
+from excel_scripts.create_excel_file import CreateExcelFile
+
+args = (
+    ("TM", "10.4", [{'Col 1': 'data 1', 'Col 2': "data2", 'Col 3': "data 3"}]),
+)
+
+
+class TestJiraProjectIssues:
+    @pytest.mark.parametrize("project, fix_version, data_list", args)
+    def test_excel_object(self, project, fix_version, data_list):
+        response = CreateExcelFile(filename=project + " " + fix_version, sheetname=fix_version)
+        assert response.filename == f'{project} {fix_version}'
+        response.create_excel_book(data_list=data_list)
