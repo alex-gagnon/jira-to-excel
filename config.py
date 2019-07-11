92d29613c647dbdbcb3dbebc7d29e1e2cac7fcd3
bokoblin-link <alexgagnon227@gmail.com>
2019-07-11 12:44:46 -0400

    Updated config to create assets directory if it doesn't exist.

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
 import os
 
 PROJECT_PATH = os.path.dirname(__file__)
-FILE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'assets')
+FILE_PATH = os.path.dirname(os.path.abspath(__file__))
+assets = 'assets'
+try:
+    os.path.join(FILE_PATH, assets)
+except FileNotFoundError:
+    os.mkdir(os.path.join(FILE_PATH, assets))
+finally:
+    FILE_PATH = os.path.join(FILE_PATH, assets)
+
 
 
 class Config:
