caed09c3805ca093856c27abb877a3e629871cf3
bokoblin-link <alexgagnon227@gmail.com>
2019-07-16 16:54:45 -0400

    Created script to base filter options off the selected Project. Added jQuery for scripts.

---
+++ client/static/dynaScript.js
@@ -0,0 +1,19 @@
+$(document).ready(() => {
+    function dynaFilters() {
+        return new Promise((reject, resolve) => {
+            let project = document.getElementById('project');
+            let filter_by = $('#filter_by');
+
+            if (project.value !== 'CUST') {
+                filter_by.empty().append(`<option value="fix_version">Fix Version</option>`);
+            } else {
+                filter_by.empty().append(`<option value="latest_version">Latest Version</option>`);
+            }
+            resolve();
+        })
+    }
+
+    $('#project').on('change', () => {
+        dynaFilters();
+    });
+});
