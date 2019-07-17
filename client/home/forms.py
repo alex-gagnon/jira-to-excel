32d5fe46110ca0ae581ad7ac3bed27d09cee9a90
bokoblin-link <alexgagnon227@gmail.com>
2019-07-17 17:03:02 -0400

    Added some temporary styling. Removed to choices from forms as they are currently not set up to filter

--- client/home/forms.py
+++ client/home/forms.py
@@ -4,16 +4,14 @@ from wtforms.fields import StringField, SelectField, SubmitField
 
 
 class Base(FlaskForm):
-    project = SelectField(label='Project: ',
+    project = SelectField(label='Project',
                           validators=[validators.DataRequired()],
                           choices=[('TM', 'Title Management'),
-                                   ('TMT', 'Title Management Training'),
-                                   ('REL', 'Release'),
                                    ('CUST', 'Customers')])
-    filter_by = SelectField(label='Filter by: ',
+    filter_by = SelectField(label='Filter by',
                             validators=[validators.DataRequired()],
                             choices=[('fix_version', 'Fix Version'),
                                      ('latest_version', 'Latest Version')])
-    version = StringField(label='Version: ',
+    version = StringField(label='Version',
                           validators=[validators.Optional()])
     generate = SubmitField(label='DOWNLOAD REPORT')
