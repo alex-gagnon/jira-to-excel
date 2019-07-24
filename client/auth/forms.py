dd11e89fe5ce3274f7a6f738c03bd07513e7c502
bokoblin-link <alexgagnon227@gmail.com>
2019-07-24 16:57:28 -0400

    Added navigation to log in and log out

--- client/auth/forms.py
+++ client/auth/forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import validators
-from wtforms.fields import StringField, SubmitField, BooleanField
+from wtforms.fields import StringField, SubmitField, BooleanField, PasswordField
 
 
 class Auth(FlaskForm):
     username = StringField(label='Username',
                            validators=[validators.DataRequired()])
-    password = StringField(label='Password',
-                           validators=[validators.DataRequired()])
+    password = PasswordField(label='Password',
+                             validators=[validators.DataRequired()])
     remember = BooleanField(label='Remember me',
                             validators=[validators.Optional()])
     login = SubmitField(label='Login')
